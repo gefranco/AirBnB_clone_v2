c2a1f7ae03942205475ab523ae812ce82a6ebdb5
German Franco F <933@holbertonschool.com>
2020-01-03 23:44:51 +0000

    [add] a Fabric script that generates a .tgz archive from the contents of the web_static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+'''
+from fabric.api import *
+import datetime
+
+env.hosts = [
+    '35.227.43.49',
+]
+env.user = "ubuntu"
+env.key_filename = './holberton'
+
+
+def do_pack():
+        '''
+        '''
+        now = datetime.datetime.now()
+        get(remote_path="/data/web_static/",
+            local_path="/versions/web_static_{}{}{}{}{}{}.tgz".format
+            (now.year, now.month, now.day, now.hour, now.minute, now.second))
