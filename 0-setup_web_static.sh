87dcb69faf9cace40bcfc720f4a89b196e124cfd
German Franco F <933@holbertonschool.com>
2020-01-03 20:49:57 +0000

    [fix] make executable

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # a Bash script that sets up your web servers for the deployment of web_static
-sudo apt-get -y update
-sudo apt-get -y install nginx
+#sudo apt-get -y update
+#sudo apt-get -y install nginx
 sudo mkdir -p /data/
 sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
