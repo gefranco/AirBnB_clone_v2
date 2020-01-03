1a4a7fc4f72a17ec3c49add67a4db00feb80c785
German Franco F <933@holbertonschool.com>
2020-01-03 21:06:09 +0000

    [fix] add exit success

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,3 +12,4 @@ sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "s/server_name _;/server_name _;\n\tlocation \/hbnb_static {\n\t\talias\/data\/web_static\/current\/;\n\t}/" /etc/nginx/sites-available/default
 sudo service nginx restart
+exit 0
