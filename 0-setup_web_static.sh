51c3db19b0d05943be9fede7f314c6b7fc72de3a
German Franco F <933@holbertonschool.com>
2020-01-03 20:31:40 +0000

    [add]initial bash script that sets up the web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+echo -e "testing..." | sudo tee /data/web_static/releases/test/index.html
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo service nginx restart
