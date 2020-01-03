6c0ee4ff74ecb21779780c50e759d12e981c6253
German Franco F <933@holbertonschool.com>
2020-01-03 20:42:56 +0000

    [fix] add comments

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # a Bash script that sets up your web servers for the deployment of web_static
+sudo apt-get -y update
+sudo apt-get -y install nginx
 sudo mkdir -p /data/
 sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
@@ -8,4 +10,5 @@ sudo mkdir -p /data/web_static/releases/test/
 echo -e "testing..." | sudo tee /data/web_static/releases/test/index.html
 sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation\/hbnb_static\/{\n\t\talias\/data\/web_static\/current\/;n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
