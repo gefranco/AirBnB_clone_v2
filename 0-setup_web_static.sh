4981708d7da14324567e3ce10738db555d25a8a7
German Franco F <933@holbertonschool.com>
2020-01-03 20:48:37 +0000

    [fix] make executable

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,5 +10,5 @@ sudo mkdir -p /data/web_static/releases/test/
 echo -e "testing..." | sudo tee /data/web_static/releases/test/index.html
 sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation\/hbnb_static\/{\n\t\talias\/data\/web_static\/current\/;n\t}\n' /etc/nginx/sites-available/default
+#sudo sed -i '38i\\tlocation\/hbnb_static\/{\n\t\talias\/data\/web_static\/current\/;n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
