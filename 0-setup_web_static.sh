229f8a76ea02fe8b66e05f776e9ce6dc81344fa7
German Franco F <933@holbertonschool.com>
2020-01-04 00:24:38 +0000

    [fix] add exit success

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,8 +8,8 @@ sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 echo -e "testing..." | sudo tee /data/web_static/releases/test/index.html
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "s/http {/http {add_header X-Served-By \$HOSTNAME;/" /etc/nginx/nginx.conf
 sudo sed -i "s/server_name _;/server_name _;\n\tlocation \/hbnb_static {\n\t\talias\/data\/web_static\/current\/;\n\t}/" /etc/nginx/sites-available/default
-sudo service nginx start
+sudo service nginx restart
