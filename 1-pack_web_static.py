653f6dff0ef9e5143c56e272cbc6fc43e2e7b2ec
German Franco F <933@holbertonschool.com>
2020-01-04 01:03:41 +0000

    [fix] time format

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,7 +2,7 @@
 '''
 '''
 from fabric.api import *
-import datetime
+from time import strftime
 
 env.hosts = [
     '35.227.43.49',
@@ -14,12 +14,11 @@ env.key_filename = './holberton'
 def do_pack():
     '''
     '''
-    now = datetime.datetime.now()
+    now = strftime("%Y%m%d%H%M%S")
     try:
         get(remote_path="/data/web_static/",
-            local_path="/versions/web_static_{}{}{}{}{}{}.tgz".format
-            (now.year, now.month, now.day, now.hour, now.minute, now.second))
+            local_path="/versions/web_static_{}.tgz".format
+            (now))
     except:
         return None
-    return "/versions/web_static_{}{}{}{}{}{}.tgz".format
-    (now.year, now.month, now.day, now.hour, now.minute, now.second)
+    return "/versions/web_static_{}.tgz".format(now)
