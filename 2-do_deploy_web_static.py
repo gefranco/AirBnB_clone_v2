a7aa9f22b80fc2a9f3212acfec32a55e8f1dc755
German Franco F <933@holbertonschool.com>
2020-01-04 16:10:50 +0000

    [fix] add /

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -37,7 +37,7 @@ def do_deploy(archive_path):
         name_alone = name_archive.replace(".tgz", "")
         put(archive_path, "/tmp/")
         run("sudo mkdir -p /data/web_static/releases/{}/".format(name_alone))
-        run("sudo tar -xvzf /tmp/{} -C /data/web_static/releases/{}".format(
+        run("sudo tar -xvzf /tmp/{} -C /data/web_static/releases/{}/".format(
             name_archive, name_alone))
         run("sudo rm /tmp/{}".format(name_archive))
         run("sudo mv /data/web_static/releases/{}/web_static/*\
