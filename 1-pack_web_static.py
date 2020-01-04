2ba08ca08747becabd70881ebc7e2fceb290d5cf
German Franco F <933@holbertonschool.com>
2020-01-04 01:33:36 +0000

    [fix] change all

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,21 +4,13 @@
 from fabric.api import *
 from time import strftime
 
-env.hosts = [
-    '35.227.43.49',
-]
-env.user = "ubuntu"
-env.key_filename = './holberton'
-
-
 def do_pack():
     '''
     '''
     now = strftime("%Y%m%d%H%M%S")
     try:
-        get(remote_path="/data/web_static/",
-            local_path="/versions/web_static_{}.tgz".format
-            (now))
+        local("mkdir -p /versions")
+        local("tar -cvzf /versions/web_static{}.tgz web_static".format(now))
     except:
         return None
     return "/versions/web_static_{}.tgz".format(now)
