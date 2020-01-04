e5917f02cbd3def802c6c41dbd9a102288e559d8
German Franco F <933@holbertonschool.com>
2020-01-04 01:36:09 +0000

    [fix] add sudo

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,8 +10,8 @@ def do_pack():
     '''
     now = strftime("%Y%m%d%H%M%S")
     try:
-        local("mkdir -p /versions")
-        local("tar -cvzf /versions/web_static{}.tgz web_static".format(now))
+        local("sudo mkdir -p /versions")
+        local("sudo tar -cvzf /versions/web_static{}.tgz web_static".format(now))
     except:
         return None
     return "/versions/web_static_{}.tgz".format(now)
