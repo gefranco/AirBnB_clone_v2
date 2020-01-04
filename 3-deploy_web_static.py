dd197292442e51e781383e67488301e311a26687
German Franco F <933@holbertonschool.com>
2020-01-04 04:14:50 +0000

    [add] a Fabric script that creates and distributes an archive to web servers

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+'''
+'''
+from fabric.api import *
+from time import strftime
+from os import path
+import sys
+env.hosts = [
+    '35.227.43.49',
+    '35.231.164.230'
+]
+#env.user = "ubuntu"
+#env.key_filename = './holberton'
+
+
+def do_pack():
+    '''
+    '''
+    now = strftime("%Y%m%d%H%M%S")
+    try:
+        local("sudo mkdir -p versions")
+        local("sudo tar -cvzf versions/web_static{}.tgz web_static".format(
+            now))
+    except:
+        return None
+    return "versions/web_static_{}.tgz".format(now)
+
+
+def do_deploy(archive_path):
+    '''
+    '''
+    if not path.exists(archive_path):
+        print("no file")
+        return False
+
+    try:
+        name_archive = archive_path.split("/")[1]
+        name_alone = name_archive.split(".")[0]
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p /data/web_static/releases/{}".format(name_alone))
+        run("sudo tar -xvzf /tmp/{} -C /data/web_static/releases/{}".format(name_archive, name_alone))
+        run("sudo rm /tmp/{}".format(name_archive))
+        run("sudo mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}".format(name_alone, name_alone))
+        run("sudo rm -rf /data/web_static/releases/{}/web_static".format(name_alone))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s /data/web_static/releases/{} /data/web_static/current".format(name_alone))
+        print("New version deployed!")
+
+        return True
+    except:
+        return False
+
+def deploy():
+    do_deploy(do_pack())
