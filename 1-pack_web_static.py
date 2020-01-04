9516afa7a7d1d4e2d4b67e85b4a9e2477e89067c
German Franco F <933@holbertonschool.com>
2020-01-04 01:40:53 +0000

    [fix] path

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,9 +10,9 @@ def do_pack():
     '''
     now = strftime("%Y%m%d%H%M%S")
     try:
-        local("sudo mkdir -p /versions")
-        local("sudo tar -cvzf /versions/web_static{}.tgz web_static".format(
+        local("sudo mkdir -p versions")
+        local("sudo tar -cvzf versions/web_static{}.tgz web_static".format(
             now))
     except:
         return None
-    return "/versions/web_static_{}.tgz".format(now)
+    return "versions/web_static_{}.tgz".format(now)
