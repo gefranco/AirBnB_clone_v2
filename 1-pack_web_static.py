9710ca6a934b2293b6756381d70027b165fcd7fa
German Franco F <933@holbertonschool.com>
2020-01-04 01:39:12 +0000

    [fix] pep8

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,7 +11,8 @@ def do_pack():
     now = strftime("%Y%m%d%H%M%S")
     try:
         local("sudo mkdir -p /versions")
-        local("sudo tar -cvzf /versions/web_static{}.tgz web_static".format(now))
+        local("sudo tar -cvzf /versions/web_static{}.tgz web_static".format(
+            now))
     except:
         return None
     return "/versions/web_static_{}.tgz".format(now)
