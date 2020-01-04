ad350da512a55b05fd10ae2d4aae8a8d0d804050
German Franco F <933@holbertonschool.com>
2020-01-04 04:05:42 +0000

    [fix] user and key by argument

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -37,13 +37,13 @@ def do_deploy(archive_path):
         name_archive = archive_path.split("/")[1]
         name_alone = name_archive.split(".")[0]
         put(archive_path, "/tmp/")
-        sudo("mkdir -p /data/web_static/releases/{}".format(name_alone))
-        sudo("tar -xvzf /tmp/{} -C /data/web_static/releases/{}".format(name_archive, name_alone))
-        sudo("rm /tmp/{}".format(name_archive))
-        sudo("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}".format(name_alone, name_alone))
-        sudo("rm -rf /data/web_static/releases/{}/web_static".format(name_alone))
-        sudo("unlink /data/web_static/current")
-        sudo("ln -s /data/web_static/releases/{} /data/web_static/current".format(name_alone))
+        run("sudo mkdir -p /data/web_static/releases/{}".format(name_alone))
+        run("sudo tar -xvzf /tmp/{} -C /data/web_static/releases/{}".format(name_archive, name_alone))
+        run("sudo rm /tmp/{}".format(name_archive))
+        run("sudo mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}".format(name_alone, name_alone))
+        run("sudo rm -rf /data/web_static/releases/{}/web_static".format(name_alone))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s /data/web_static/releases/{} /data/web_static/current".format(name_alone))
         return True
     except:
         return False
