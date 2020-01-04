82e92f99d13294506c18748b52192fcfa7400ac4
German Franco F <933@holbertonschool.com>
2020-01-04 00:37:13 +0000

    [fix] return

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,6 +10,5 @@ sudo mkdir -p /data/web_static/releases/test/
 echo -e "testing..." | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "s/http {/http {add_header X-Served-By \$HOSTNAME;/" /etc/nginx/nginx.conf
 sudo sed -i "s/server_name _;/server_name _;\n\tlocation \/hbnb_static {\n\t\talias\/data\/web_static\/current\/;\n\t}/" /etc/nginx/sites-available/default
 sudo service nginx restart
