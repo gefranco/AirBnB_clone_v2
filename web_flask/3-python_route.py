579b22c4cc4ccb60aeed700b7fa2dea053735288
German Franco F <933@holbertonschool.com>
2020-01-21 23:32:53 +0000

    [fix]pep8

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,18 +1,23 @@
+#!/usr/bin/python3
 from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
 
 @app.route('/')
 def hello_HBNB():
     return "Hello HBNB!"
 
+
 @app.route('/hbnb')
 def hbnb():
     return "HBNB"
 
+
 @app.route('/c/<text>')
 def c(text):
     return 'C %s' % text.replace('_', ' ')
+
 
 @app.route('/python/', defaults={'text': 'is cool'})
 @app.route('/python/<text>')
