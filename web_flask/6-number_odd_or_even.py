2c5880775db033fe467366dd3353d4f5b4c9cc93
German Franco F <933@holbertonschool.com>
2020-01-21 23:39:52 +0000

    [fix]pep8

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,38 +1,44 @@
+#!/usr/bin/python3
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def hello_HBNB():
     return "Hello HBNB!"
+
 
 @app.route('/hbnb')
 def hbnb():
     return "HBNB"
 
+
 @app.route('/c/<text>')
 def c(text):
     return 'C %s' % text.replace('_', ' ')
+
 
 @app.route('/python/', defaults={'text': 'is cool'})
 @app.route('/python/<text>')
 def python(text):
     return 'Python %s' % text.replace('_', ' ')
 
+
 @app.route('/number/<int:n>')
 def number(n):
     return '%d is a number' % n
+
 
 @app.route('/number_template/<int:n>')
 def number_template(n):
     return render_template('5-number.html', n=n)
 
+
 @app.route('/number_odd_or_even/<int:n>')
 def number_odd_or_even(n):
     return render_template('6-number_odd_or_even.html', n=n)
-
-
 
 
 if __name__ == '__main__':
