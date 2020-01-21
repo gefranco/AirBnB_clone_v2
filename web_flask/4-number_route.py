79e7bff981039cd1ef8dd9cff16452df30626d42
German Franco F <933@holbertonschool.com>
2020-01-21 23:34:43 +0000

    [fix] pep8

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,23 +1,29 @@
+#!/usr/bin/python3
 from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
 
 @app.route('/')
 def hello_HBNB():
     return "Hello HBNB!"
 
+
 @app.route('/hbnb')
 def hbnb():
     return "HBNB"
+
 
 @app.route('/c/<text>')
 def c(text):
     return 'C %s' % text.replace('_', ' ')
 
+
 @app.route('/python/', defaults={'text': 'is cool'})
 @app.route('/python/<text>')
 def python(text):
     return 'Python %s' % text.replace('_', ' ')
+
 
 @app.route('/number/<int:n>')
 def number(n):
