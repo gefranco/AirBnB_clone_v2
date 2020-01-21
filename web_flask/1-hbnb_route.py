cc3b6b11e858223c44e3b477ddbb68302aaf6fb8
German Franco F <933@holbertonschool.com>
2020-01-21 23:22:59 +0000

    [fix]pep8

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,10 +1,13 @@
+#!/usr/bin/python3
 from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def hello_HBNB():
     return "Hello HBNB!"
+
 
 @app.route('/hbnb')
 def hbnb():
